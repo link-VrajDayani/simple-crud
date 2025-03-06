55c9e96064c32278b503b96d016be797cc7dbe8c
link-VrajDayani <vrajdayani2@gmail.com>
2025-03-06 14:02:15 +0530

    My Simple Crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,62 +4,66 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\checkModel;
-
+use App\Models\City;
 
 class UserController extends Controller
 {
     public function view()
     {
-        return View('welcome');
+        return view('welcome');
     }
-    public function firstview()
-    {
-        $cities = ['Mumbai', 'Ahmedabad', 'bengaluru', 'Chennai', 'Hyderabad'];
-        return view('first', compact('cities'));
-    }
+
+
+public function firstview()
+{
+    $cities = City::pluck('name');
+    return view('first', compact('cities'));
+}
+
     public function secondview()
     {
         $allData = checkModel::all();
-        // dd($allData); 
-        // foreach ($allData as $data) {
-        //     $data->hobbies = json_decode($data->hobbies, true );
-        // }
-        return View('second', compact('allData'));
+        return view('second', compact('allData'));
     }
+
     public function dataCreate(Request $request)
-    {
-        // dd($request->all());
-        $hobbies = $request->hobbies ? implode(',', $request->hobbies) : '';
+{
+    $hobbies = $request->hobbies ? implode(',', $request->hobbies) : '';
 
+    $record = checkModel::find($request->id);
+    $fileName = $record ? $record->file : null;
+
+    if ($request->hasFile('file')) {
         $image = $request->file;
-        $fileNAme = time().$image -> getClientOriginalName();
-        $image -> move(public_path('Images/') , $fileNAme);
+        $fileName = time() . $image->getClientOriginalName();
+        $image->move(public_path('Images/'), $fileName);
+    }
 
-        checkModel::updateorcreate([
-            "id" => $request->id 
-        ],[
+    checkModel::updateOrCreate(
+        ["id" => $request->id],
+        [
             "name" => $request->name,
             "surname" => $request->surname,
             "gender" => $request->gender,
             "hobbies" => $hobbies,
-            // "hobbies" => json_encode($request->hobbies ?? []),
-            'city' => $request->city,
-            'file' => $fileNAme,
+            "city" => $request->city,
+            "file" => $fileName,
+        ]
+    );
 
-        ]);
-        return redirect()->route('second')->with('success', 'Form submitted successfully!');
-    }
+    return redirect()->route('second')->with('success', 'Form submitted successfully!');
+}
 
     public function dataSearch(Request $request)
     {
         $search = $request->input('search');
     
         if ($search) {
-        $allData = CheckModel::where('name', 'like', "%{$search}%")
+            $allData = checkModel::where('name', 'like', "%{$search}%")
                 ->orWhere('surname', 'like', "%{$search}%")
                 ->get();
         } else {
-            $allData = CheckModel::all(); 
+            $allData = checkModel::all();
         }
         
         return view('second', compact('allData'));
@@ -67,23 +71,18 @@ class UserController extends Controller
 
     public function dataDelete($id)
     {
-        // dd($id);
-
         $deleteRecord = checkModel::find($id);
-        // dd($deleteRecord);
-        $deleteRecord -> delete();
+        $deleteRecord->delete();
 
         return redirect()->back();
     }
 
     public function dataEdit($id)
-    {
-        $editRecord = checkModel::find($id);
-        $editRecord->hobbies = explode(',', $editRecord->hobbies ?? '');
-        $cities = ['Mumbai', 'Ahmedabad', 'bengaluru', 'Chennai', 'Hyderabad'];
-        return view('first', compact('editRecord', 'cities'));
-    }
+{
+    $editRecord = checkModel::find($id);
+    $editRecord->hobbies = explode(',', $editRecord->hobbies ?? '');
+    $cities = City::pluck('name');
 
+    return view('first', compact('editRecord', 'cities'));
 }
-
-?>
+}
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/second' , [UserController::class,'secondview']) -> name('second');
 Route::post('/dataCreate', [UserController::class, 'dataCreate']) -> name('dataCreate');
 Route::get('/dataSearch', [UserController::class, 'dataSearch'])->name('dataSearch');
 Route::get('/dataDelete/{id}', [UserController::class, 'dataDelete']) -> name('dataDelete');
-Route::get('/dataEdit/{id}', [UserController::class, 'dataEdit'])->name('dataEdit');
+Route::get('/dataEdit/{id}', [UserController::class, 'dataedit'])->name('dataEdit');
 
 
 ?>
